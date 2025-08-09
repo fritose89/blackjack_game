e3ae71eebeb8276777f5490a741cb824eb0f3c22
fritose89 <paul.prattini@gmail.com>
2025-08-09 13:40:49 -0500

    Create Card, Deck, Rank, and Suit classes
    Add enums for rank and suit values. Implement deck shuffle and
    card/deck generation for a user-specified number of decks.

---
+++ src/main/java/com/fritose89/blackjack/Suit.java
@@ -0,0 +1,13 @@
+public enum Suit{
+	HEARTS("Hearts"), DIAMONDS("Diamonds"), CLUBS("Clubs"), SPADES("Spades");
+
+	private final String symbol;
+
+	Suit(String symbol){
+		this.symbol = symbol;
+	}
+
+	public String getSymbol(){
+		return symbol;
+	}
+}
